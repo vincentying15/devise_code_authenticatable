b98d83eddc96cca84da78c4c95cdbe831e223ede
vincentying15 <vincent_ying_live@outlook.com>
2020-02-18 12:03:37 +0800

    Fix mailer error

--- lib/devise_code_authenticatable/mailer.rb
+++ lib/devise_code_authenticatable/mailer.rb
@@ -1,5 +1,10 @@
 module DeviseCodeAuthenticatable
   module Mailer
+    extend ActiveSupport::Concern
+
+    included do
+      default from: ENV["DEVISE_MAILER_SENDER"]
+    end
 
     # deliver a mail containing login code
     def code_login_instructions(record, code, opts = {})
