819ed1320d457260f31a1556e74ff594eb3efef4
vincentying15 <vincent_ying_live@outlook.com>
2020-02-21 15:27:32 +0800

    Add sessions routes, can remove :database_authenticatable now

--- lib/devise_code_authenticatable/routes.rb
+++ lib/devise_code_authenticatable/routes.rb
@@ -4,6 +4,15 @@ module ActionDispatch::Routing
   protected
 
     def devise_login_code(mapping, controllers)
+      resource :session, only: [], path: "",
+      controller: controllers[:sessions] do
+        collection do
+          get :new, path: mapping.path_names[:sign_in], as: :new
+          post :create, path: mapping.path_names[:sign_in]
+          delete :destroy, path: mapping.path_names[:sign_out], as: :destroy
+        end
+      end
+
       resources :login_codes, only: [:show],
       controller: controllers[:login_codes] do
         member do
