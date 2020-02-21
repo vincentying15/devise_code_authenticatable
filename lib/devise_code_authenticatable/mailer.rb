e9fa3350aa9fd44c7daa8bd5231dc19be223db48
vincentying15 <vincent_ying_live@outlook.com>
2020-02-21 17:23:34 +0800

    Add code to mail subject

--- lib/devise_code_authenticatable/mailer.rb
+++ lib/devise_code_authenticatable/mailer.rb
@@ -9,6 +9,7 @@ module DeviseCodeAuthenticatable
     # deliver a mail containing login code
     def code_login_instructions(record, code, opts = {})
       @code = code
+      opts[:subject] = "#{@code} is your login code"
       devise_mail(record, :login_code, opts)
     end
   end
